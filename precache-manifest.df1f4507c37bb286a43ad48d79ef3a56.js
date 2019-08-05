f9116269cf0c93233fa41a6d87ab51f0cc01c179
@bartosz.lazarski <b4rt99@gmail.com>
2019-08-05 03:55:23 +0200

    Updates

---
+++ precache-manifest.df1f4507c37bb286a43ad48d79ef3a56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14be13977aacb618556248d9bd3a91b",
+    "url": "/snek-ui/index.html"
+  },
+  {
+    "revision": "d6a60b81db66997bfcf9",
+    "url": "/snek-ui/static/css/main.7a532d21.chunk.css"
+  },
+  {
+    "revision": "67d7e199f2c3ccf017f7",
+    "url": "/snek-ui/static/js/2.93a5a3bd.chunk.js"
+  },
+  {
+    "revision": "d6a60b81db66997bfcf9",
+    "url": "/snek-ui/static/js/main.c2950e59.chunk.js"
+  },
+  {
+    "revision": "08cefb84b66305d38279",
+    "url": "/snek-ui/static/js/runtime~main.75547a82.js"
+  },
+  {
+    "revision": "a671af002069bc93c081b11fc47593f4",
+    "url": "/snek-ui/static/media/P1010799-hd.a671af00.jpg"
+  },
+  {
+    "revision": "662d72190211d95944dd36b69ab20dac",
+    "url": "/snek-ui/static/media/P1010799-m.662d7219.jpg"
+  },
+  {
+    "revision": "6e8786a8b9f690ab6043c78ea69327a4",
+    "url": "/snek-ui/static/media/snake.6e8786a8.svg"
+  }
+]);
