eb88346d487593af826af6547b4e82d21f34ad98
@bartosz.lazarski <b4rt99@gmail.com>
2019-09-16 01:27:29 +0200

    issue 9 update

---
+++ src/components/pages/Landing/LastSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function LastSection() {
+    return (
+        <section class="section-contact flex-box">
+            <i class="fas fa-sign-in-alt"></i>
+            <h2>Install SnekUI now</h2>
+            <span>Fast and lightweight library for easy building any website you want, includes BEM naming style,
+                pre-done themes and support for custom definitions.</span>
+            <span></span>
+            <a href="/docs">
+                <div class="ui-button button-color--alt">get started now! <i class="fas fa-sign-in-alt"></i> </div>
+            </a>
+        </section>
+    )
+}
--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import Main from './Main'
+import LastSection from './LastSection'
 
 import * as CONST from '../../../constants/static'
 
@@ -10,6 +11,7 @@ export default class index extends Component {
             <div>
                 <Header version={CONST.VERSION} />
                 <Main />
+                <LastSection />
             </div>
         )
     }
